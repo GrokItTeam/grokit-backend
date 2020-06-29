9b96a07c9e2800aa16673c7752069797d2685d4c
Saoirse Bryar <smbryar@gmail.com>
2020-06-29 09:03:29 +0100

    Implement GET projects using userId

--- handler.js
+++ handler.js
@@ -1,18 +1,39 @@
-'use strict';
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const app = express();
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+app.use(cors());
+app.use(bodyParser.json());
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "grokit"
+});
+
+// projects table
+
+app.get("/projects", function (req, res) {
+  const userIdValue = req.query.userId;
+  const queryGet = "SELECT * FROM projects WHERE userId = ?;";
+  connection.query(queryGet, userIdValue, function (error, data) {
+    if (error) {
+      console.log("Error fetching projects", error);
+      res.status(500).json({
+        error: error
+      })
+    }
+    else {
+      res.status(200).json({
+        projects: data
+      })
+    }
+  })
+});
+
+module.exports.projects = serverless(app);
