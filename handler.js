23670e4aa2e572120409d3a87c64c09792992ae2
Saoirse Bryar <smbryar@gmail.com>
2020-06-30 11:06:51 +0100

    Combine project and skill get calls to happen together

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const moment = require("moment");
 
-const {skillChooser} = require('./skillChooser');
+const { skillChooser } = require('./skillChooser');
 
 const app = express();
 
@@ -19,12 +19,14 @@ const connection = mysql.createConnection({
   database: "grokit"
 });
 
-// projects table
+// get projects and skills
 
 app.get("/projects", function (req, res) {
   const userIdValue = req.query.userId;
-  const queryGet = "SELECT * FROM projects WHERE userId = ?;";
-  connection.query(queryGet, userIdValue, function (error, data) {
+  const queryGetProjects = "SELECT * FROM projects WHERE userId = ?;";
+  const queryGetSkills = "SELECT * FROM skills WHERE projectId IN (?);";
+
+  connection.query(queryGetProjects, [userIdValue], function (error, projectData) {
     if (error) {
       console.log("Error fetching projects", error);
       res.status(500).json({
@@ -32,12 +34,31 @@ app.get("/projects", function (req, res) {
       })
     }
     else {
-      res.status(200).json({
-        projects: data
-      })
+      let projectIds = projectData.map(project => project.projectId);
+      console.log(projectIds);
+      connection.query(queryGetSkills, [projectIds], function (error, skillData) {
+        if (error) {
+          console.log("Error fetching skills", error);
+          res.status(500).json({
+            error: error,
+          });
+        }
+        else {
+          const data = projectData.map((project) => {
+            const skills = skillData.filter((skill) => skill.projectId === project.projectId);
+            project.skills = skills;
+            return project;
+          });
+          res.status(200).json({
+            projects: data,
+          });
+        }
+      });
     }
-  })
+  });
 });
+
+
 
 //get skill to do by projectId
 app.get("/projects/:projectId/skillToDo", function (req, res) {
@@ -51,30 +72,9 @@ app.get("/projects/:projectId/skillToDo", function (req, res) {
       })
     }
     else {
-      let skillToDo = skillChooser(data,moment());
+      let skillToDo = skillChooser(data, moment());
       res.status(200).json({
         skillToDo
-      })
-    }
-  })
-});
-
-
-// skills table
-
-app.get("/skills", function (req, res) {
-  const projectId = req.query.projectId;
-  const queryGet = "SELECT * FROM skills WHERE projectId = ?;";
-  connection.query(queryGet, projectId, function (error, data) {
-    if (error) {
-      console.log("Error fetching skills", error);
-      res.status(500).json({
-        error: error
-      })
-    }
-    else {
-      res.status(200).json({
-        skills: data
       })
     }
   })
