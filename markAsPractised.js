d34575a2e6abd92ef8f03700407b45d7bbaeb192
Saoirse Bryar <smbryar@gmail.com>
2020-07-05 13:18:13 +0100

    Add loop and switch for different difficulties

--- markAsPractised.js
+++ markAsPractised.js
@@ -1,16 +1,31 @@
 const moment = require('moment');
 
-function markAsPractised(skill,today) {
+function markAsPractised(skill,today,difficulty) {
     const updatedSkill = Object.assign({},skill);
     if (!updatedSkill.started) {
         updatedSkill.lastGap0 = 0;
         updatedSkill.lastGap1 = 1;
         updatedSkill.started = 1;
     }
-    let nextGap = updatedSkill.lastGap0 + updatedSkill.lastGap1;
-    updatedSkill.nextDate = moment(today).add(nextGap, "days").format("YYYY-MM-DD");
-    updatedSkill.lastGap0 = updatedSkill.lastGap1;
-    updatedSkill.lastGap1 = nextGap;
+
+    switch(difficulty) {
+        case "easy":
+            loops = 0;
+            break;
+        case "medium":
+            loops = 1;
+            break;
+        case "hard":
+            loops = 2;
+    }
+
+    for (let i = 0; i<loops; i++) {
+        nextGap = updatedSkill.lastGap0 + updatedSkill.lastGap1;
+        updatedSkill.lastGap0 = updatedSkill.lastGap1;
+        updatedSkill.lastGap1 = nextGap;
+    }
+    updatedSkill.nextDate = moment(today).add(updatedSkill.lastGap1, "days").format("YYYY-MM-DD");
+    
     return updatedSkill;
 }
 
